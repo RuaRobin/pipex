0b93be2c1203d958f78b97281f91ce6a76c85f7b
RuaRobin <roaarobin3@gmail.com>
2026-01-21 16:03:47 +0300

    adding

--- main.c
+++ main.c
@@ -1,45 +1,79 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
-//infile -> 3
-
-infile = open("infile", FLAG);
-outfile = open("infile", FLAG);
+int main(int argc, char **argv, char **envp)
+{
+if (argc != 5)
+{
+    write(2,"Not enough arguments \n ./pipex file1 cmd1 cmd2 file2 \n", 56);
+    return 1;
+}
 int fd[2];
-//[4,5]
+int infile = read_files(argv[1],0);
+int outfile = read_files(argv[3],1);
+char *cmd1 = findcommand(argv[2], envp);
+char * cmd2 = findcommand(argv[4],envp);
+if (!cmd2)
+{
+    write(2,"Command doesnt exist \n",27);
+    return 127;
+}
+if (!cmd1)
+    write(2,"Command doesnt exist \n",27);
 if(pipe(fd) == -1)
 {
-  //  perror()
+    perror("pipe");
+    close(infile);
+    close(outfile);
     return;
 }
-int id = fork();
-//if id==-1
-if (id==0)
+if (infile > 0)
 {
-    if(cmd1 == NULL)
-    {
-        // write error message
-        // exite
-    }
-    // close readig end of the pipe
-    // dup2 (infile, 0)
-    // dup2 (fd[1], 1)
-    // execve (cmd1)
+int id = fork();
+if (id == -1 )
+{
+    perror("fork");
+    close(infile);
+    close(outfile);
+    close(fd[0]);
+    close(fd[1]);
+    return;
 }
+if (id == 0)
+{
+    close(fd[0]);    // close readig end of the pipe
+    dup2 (infile, 0);
+    dup2 (fd[1], 1);
+    if (execve(cmd1, infile,envp) == -1)
+        write(2,"An error has occured \n",23);
+}
+}
+else 
+    write(2, "Error: cannot open infile\n", 27);
 int id2 = fork();
-
+if (id2==-1)
+{
+    perror();
+    close(infile);
+    close(outfile);
+    close(fd[0]);
+    close(fd[1]);
+    return;
+}
 if (id2 ==0)
 {
-    if(cmd2 == NULL)
-    {
-        // write error message
-        // exite
-    }
-    // close writin end of the pipe
-    // dup2 (fd[0] , 0)
-    // dup2 (outfile , 1)
-    // execve (cmd1) 
-}
+    close(fd[1]);    // close reading end of the pipe
+    dup2 (fd[0] , 0);
+    dup2 (outfile , 1);
+    if (execve(cmd2, infile,envp) == -1)
+        write(2,"An error has occured \n",23);}
 //close the write end of the pipe 
-waitpid(id1)
-waitpid(id2)
+close(fd[0]);
+close(fd[1]);
+waitpid(id);
+waitpid(id2);
 // take status
 // the exit code should be the secound cmd exit code meaning the secoind status, use wifexit
+return 0;
+}
